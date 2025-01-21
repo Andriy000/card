d489cde7b774512273b6a0adfbf8757ddd7a5527
Andriy000 <>
2025-01-21 23:57:41 +0200

    add

--- server.js
+++ server.js
@@ -2,21 +2,17 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './db/connectToMongo.js';
 import bodyParser from 'body-parser';
-import cardRoute from './routes/card.route.js'
+import CardRoute from './routes/card.route.js'
 import cors from 'cors';
 
 dotenv.config();
 const app = express();
-app.use(cors({
-  origin: '*',
-  methods: 'GET, POST',
-  allowedHeaders: 'Content-Type, Authorization', 
-}));
+app.use(cors())
 app.use(bodyParser.json());
 
 
 connectDB();
-app.use('/api/cards', cardRoute);
+app.use('/api/cards', CardRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
