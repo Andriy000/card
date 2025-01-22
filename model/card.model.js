421128ea656d5a6c30dc1ac22a3935753f927abf
Andriy000 <>
2025-01-22 08:31:24 +0200

    fix

--- model/card.model.js
+++ model/card.model.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
 const CardSchema = new mongoose.Schema({
-  id : {type: String, required: true },
+  id : { type: String, required: true },
+  title : { type: String, required: false },
   type: { type: String, enum: ['villa', 'house', 'apartment'], required: true },
   price: { type: String, required: true },
   owner: {
     author: { type: String, required: true },
     location: { type: String, required: true }
   },
-  badge: { type: String, enum: ['new house', 'best deals', 'popular'], required: true },
+  badge: { type: String, enum: ['new-house', 'best-deals', 'popular'], required: true },
 }, { versionKey: false });
 
 const Card = mongoose.model('Card', CardSchema);
