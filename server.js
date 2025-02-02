8544869910a024b4c5227e8132fd0935ba25267c
Andriy000 <>
2025-02-02 13:38:43 +0200

    change_routes

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json());
 
 connectDB();
 app.use('/api/cards', CardRoute);
-app.use('/api/cards/second', CardSecondRoute);  
-app.use('/api/cards/third', CardThirdRoute);  
+app.use('/api/cards', CardSecondRoute);  
+app.use('/api/cards', CardThirdRoute);  
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
